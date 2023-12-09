54625937dafeb81c1bb0c75578829e0d5e6772f8
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-09 17:00:36 -0500

    added matrix scalar multiplication

--- src/Linear.hs
+++ src/Linear.hs
@@ -2,9 +2,16 @@ module Linear where
 
 type Matrix dataType = [[dataType]]
 
-addMatrices :: Num dataType => 
-    Matrix dataType -> 
-    Matrix dataType -> 
+addMatrices :: Num dataType =>
+    Matrix dataType ->
+    Matrix dataType ->
     Matrix dataType
 addMatrices matrixA matrixB = 
     zipWith(zipWith (+)) matrixA matrixB
+
+scaleMatrix :: Num dataType =>
+           dataType ->
+    Matrix dataType ->
+    Matrix dataType
+scaleMatrix scalar matrix =
+    map(map(scalar*)) matrix
