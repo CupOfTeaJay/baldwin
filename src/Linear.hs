691f752c6b03c0260717393cd6986eb69e1b196f
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-17 17:35:12 -0500

    Implemented multMat

--- src/Linear.hs
+++ src/Linear.hs
@@ -6,21 +6,25 @@ addMat :: Num dataType =>
     Matrix dataType ->
     Matrix dataType ->
     Matrix dataType
-addMat matA matB = zipWith(zipWith (+)) matA matB
+addMat matA matB = 
+    zipWith(zipWith (+)) matA matB
 
--- multMat :: Num dataType =>
---     Matrix dataType ->
---     Matrix dataType ->
---     Matrix dataType
--- multMat matA matB =
+multMat :: Num dataType =>
+    Matrix dataType ->
+    Matrix dataType ->
+    Matrix dataType
+multMat matA matB = 
+    map(\row -> map(\col -> sum(zipWith(*) row col)) (transposeMat matB)) matA
 
 scaleMat :: Num dataType =>
            dataType ->
     Matrix dataType ->
     Matrix dataType
-scaleMat scalar mat = map(map(scalar*)) mat
+scaleMat scalar mat = 
+    map(map(scalar*)) mat
     
 transposeMat :: Num dataType =>
     Matrix dataType ->
     Matrix dataType
-transposeMat mat = foldr(zipWith(:)) (repeat []) mat
+transposeMat mat = 
+    foldr(zipWith(:)) (repeat []) mat
