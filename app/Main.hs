2ba3677899455ab4502a6d30cae8fc373c6b8d3d
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-17 16:44:37 -0500

    Fixed indentation

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,16 @@ import qualified Linear
 main :: IO ()
 main = do
 
-  let matrixA = [[1, 3, 5]]
-  let matrixB = [[2, 4, 6]]
+    let matrixA = [[1, 3, 5],
+                   [5, 7, 9],
+                   [2, 4, 6]]
 
-  putStrLn "Adding to matrices..."
-  print (Linear.addMat matrixA matrixB)
+    let matrixB = [[2, 4, 6],
+                   [8, 10, 12],
+                   [3, 5, 7]]
+
+    putStrLn "Adding to matrices..."
+    print (Linear.addMat matrixA matrixB)
+
+    putStrLn "Transposing a matrix..."
+    print (Linear.transposeMat matrixB)
