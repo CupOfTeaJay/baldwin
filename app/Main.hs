ab357efd5e789f2041beb37ee3f7e391edd7999b
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-19 15:51:08 -0500

    Updated Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,6 @@
 module Main where
 
-import qualified Linear
-
 main :: IO ()
 main = do
 
-    let matrixA = [[1, 3, 5],
-                   [5, 7, 9],
-                   [2, 4, 6]]
-
-    let matrixB = [[2, 4, 6],
-                   [8, 10, 12],
-                   [3, 5, 7]]
-
-    putStrLn "Adding to matrices..."
-    print (Linear.matSum matrixA matrixB)
-
-    putStrLn "Transposing a matrix..."
-    print (Linear.matTrans matrixB)
+putStrLn("Dummy Text")
