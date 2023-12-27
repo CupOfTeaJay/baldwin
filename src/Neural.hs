fb7eee173ca1b7bdb1556a6f7b5234f9ed320035
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-27 09:29:41 -0500

    Removed trailing whitespace

--- src/Neural.hs
+++ src/Neural.hs
@@ -2,12 +2,12 @@ module Neural where
 
 import Linear
 
-data DenseLayer dataType = DenseLayer {weights :: Linear.Matrix dataType, 
+data DenseLayer dataType = DenseLayer {weights :: Linear.Matrix dataType,
                                        biases :: Linear.Vector dataType}
 
-calcLayer :: Num dataType 
-    => Linear.Vector dataType 
-    -> DenseLayer    dataType 
+calcLayer :: Num dataType
+    => Linear.Vector dataType
+    -> DenseLayer    dataType
     -> Linear.Vector dataType
 calcLayer ingress layer =
     Linear.vecSum (Linear.matVecMul (weights layer) ingress) (biases layer)
