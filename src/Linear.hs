1ed97fb7ece99ceaab77ad9d9c62c759952b8467
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-27 09:27:04 -0500

    Reformatted code

--- src/Linear.hs
+++ src/Linear.hs
@@ -3,43 +3,43 @@ module Linear where
 type Vector dataType = [dataType]
 type Matrix dataType = [Vector dataType]
 
-matMul :: Num dataType =>
-    Matrix dataType ->
-    Matrix dataType ->
-    Matrix dataType
+matMul :: Num dataType 
+    => Matrix dataType 
+    -> Matrix dataType 
+    -> Matrix dataType
 matMul matA matB = 
     map(\row -> map(\col -> sum(zipWith(*) row col)) (matTrans matB)) matA
 
-matScale :: Num dataType =>
-           dataType ->
-    Matrix dataType ->
-    Matrix dataType
+matScale :: Num dataType 
+    =>        dataType 
+    -> Matrix dataType 
+    -> Matrix dataType
 matScale scalar mat = 
     map(map(scalar*)) mat
 
-matSum :: Num dataType =>
-    Matrix dataType ->
-    Matrix dataType ->
-    Matrix dataType
+matSum :: Num dataType 
+    => Matrix dataType 
+    -> Matrix dataType 
+    -> Matrix dataType
 matSum matA matB = 
     zipWith(zipWith(+)) matA matB
     
-matTrans :: Num dataType =>
-    Matrix dataType ->
-    Matrix dataType
+matTrans :: Num dataType 
+    => Matrix dataType 
+    -> Matrix dataType
 matTrans mat = 
     foldr(zipWith(:)) (repeat []) mat
 
-matVecMul :: Num dataType =>
-    Matrix dataType ->
-    Vector dataType ->
-    Vector dataType
+matVecMul :: Num dataType 
+    => Matrix dataType 
+    -> Vector dataType 
+    -> Vector dataType
 matVecMul mat vec =
     map(\row -> sum(zipWith(*) row vec)) mat
 
-vecSum :: Num dataType =>
-    Vector dataType ->
-    Vector dataType ->
-    Vector dataType
+vecSum :: Num dataType 
+    => Vector dataType 
+    -> Vector dataType 
+    -> Vector dataType
 vecSum vecA vecB =
     zipWith(+) vecA vecB
--- src/Neural.hs
+++ src/Neural.hs
@@ -5,9 +5,9 @@ import Linear
 data DenseLayer dataType = DenseLayer {weights :: Linear.Matrix dataType, 
                                        biases :: Linear.Vector dataType}
 
-calcLayer :: Num dataType =>
-    Linear.Vector dataType ->
-    DenseLayer dataType ->
-    Linear.Vector dataType
+calcLayer :: Num dataType 
+    => Linear.Vector dataType 
+    -> DenseLayer    dataType 
+    -> Linear.Vector dataType
 calcLayer ingress layer =
     Linear.vecSum (Linear.matVecMul (weights layer) ingress) (biases layer)
