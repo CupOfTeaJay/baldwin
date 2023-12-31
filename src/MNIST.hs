1345e79100e6d85254aa5b94a5449c87029875da
CupOfTeaJay <tjgiven17@protonmail.com>
2023-12-31 10:59:47 -0500

    trying to load train data

--- src/MNIST.hs
+++ src/MNIST.hs
@@ -2,6 +2,28 @@ module MNIST where
 
 import Linear
 
-loadTrainingData :: String -> IO String
-loadTrainingData filepath =
-    readFile filepath
+parentDir :: String
+parentDir = 
+    "../_datasets/MNIST/"
+
+trainImages :: String
+trainImages =
+    parentDir ++ "train-images-idx3-ubyte"
+
+trainLabels :: String
+trainLabels = 
+    parentDir ++ "train-labels-idx1-ubyte"
+
+testImages :: String
+testImages = 
+    parentDir ++ "t10k-images-idx3-ubyte"
+
+testLabels :: String
+testLabels =
+    parentDir ++ "t10k-labels-idx1-ubyte"
+
+loadTrainData :: IO (String, String)
+loadTrainData = do
+    images <- readFile trainImages
+    labels <- readFile trainLabels
+    return (images, labels)
